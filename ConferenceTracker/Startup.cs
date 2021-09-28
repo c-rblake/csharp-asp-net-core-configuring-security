5105ae9bb93858911803af00c8d063b16b700008
Christoper <c.blake@hotmail.se>
2021-09-28 15:25:54 +0200

    Init - Startup Configure and ConfigureServices Added security. Controller Added Logging

--- ConferenceTracker/Startup.cs
+++ ConferenceTracker/Startup.cs
@@ -9,11 +9,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using Microsoft.Extensions.Logging;
 
 namespace ConferenceTracker
 {
     public class Startup
     {
+        private readonly string _allowedOrigins = "_allowedOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -29,22 +31,52 @@ namespace ConferenceTracker
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("ConferenceTracker"));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.Configure<CookiePolicyOptions>(options=>
+                {
+                    options.CheckConsentNeeded = context => true; options.MinimumSameSitePolicy = SameSiteMode.None;
+                }); // You'll need to change them based on what cookies you use and what they store
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddTransient<IPresentationRepository, PresentationRepository>();
             services.AddTransient<ISpeakerRepository, SpeakerRepository>();
+            services.AddCors(opt=>
+                {
+                    opt.AddPolicy(_allowedOrigins,
+                                    builder => { builder.WithOrigins("http.//pluralsight.com"); });
+                    //(This is specifying the name of our CORS policy, and providing what domains will be permitted)
+                });
+            SecretMessage = Configuration["SecretMessage"];
+            //Normally, you'd use this to contain things like connection strings.
+            //However, since we're using an InMemory database,
+            //this is simply being used as an example, and serves no functional purpose.
+
         }
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {            
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                logger.LogInformation("Environment is in development");
+            }
+            else
+            { 
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
             using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             using (var context = scope.ServiceProvider.GetService<ApplicationDbContext>())
                 context.Database.EnsureCreated();
 
-            app.UseStaticFiles();
+            app.UseCors(_allowedOrigins);
 
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
             app.UseRouting();
 
             app.UseAuthentication();
